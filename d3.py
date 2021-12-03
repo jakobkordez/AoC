ac9a43e6864e24fb002051a48b2d2d53fe8984bb
Jakob Kordež <jakobkordez1999@gmail.com>
2021-12-03 15:59:32 +0100

    Add Day 3

---
+++ d3.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+with open('i3.txt') as file:
+    data = np.array([list(a.strip()) for a in file.readlines()], dtype=np.int0)
+
+bits = data.shape[1]                # Number of bits
+bp = 2 ** np.arange(bits)[::-1]     # Helper array [.., 8, 4, 2, 1]
+
+# Part 1
+g = np.array([np.argmax(np.bincount(data[:, i])) for i in range(bits)])
+print(sum(g * bp) * sum((1 - g) * bp))
+
+# Part 2
+ox = np.array(data)
+for i in range(bits):
+    bc = np.bincount(ox[:, i], minlength=2)
+    ox = ox[np.where(ox[:, i] == (1 if bc[1] >= bc[0] else 0))]
+
+coo = np.array(data)
+for i in range(bits):
+    bc = np.bincount(coo[:, i], minlength=2)
+    coo = coo[np.where(coo[:, i] == (1 if bc[1] < bc[0] else 0))]
+    if len(coo) == 1:
+        break
+
+print(sum(ox[0] * bp) * sum(coo[0] * bp))
