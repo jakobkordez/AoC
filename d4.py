5ae258c289cd52c082ae30f639924af76cc28dab
Jakob Kordež <jakobkordez1999@gmail.com>
2021-12-04 20:44:15 +0100

    Optimize Day 4

--- d4.py
+++ d4.py
@@ -8,6 +8,21 @@ nums = np.array(list(map(int, raw[0].split(','))))
 boards = [np.array([list(map(int, (re.split(r'\s+', row.strip())))) for row in b.split('\n')])
           for b in raw[1:]]
 
+
+def getScore(b, i):
+    return (sum(b.flatten()) -
+            sum(np.intersect1d(b.flatten(), nums[:i]))) * nums[i - 1]
+
+
+# Big brain way
+boardWins = [min([max(np.isin(nums, rc).nonzero()[0])
+                  for rc in np.concatenate([b, b.T])]) for b in boards]
+first = np.argmin(boardWins)
+last = np.argmax(boardWins)
+print('Part 1:', getScore(boards[first], boardWins[first] + 1))
+print('Part 2:', getScore(boards[last], boardWins[last] + 1))
+
+# Dumb way
 score = None
 wb = [False] * len(boards)
 for i in range(5, len(nums)):
@@ -17,8 +32,7 @@ for i in range(5, len(nums)):
         b = boards[bi]
         for j in range(5):
             if np.all(np.isin(b[j], nums[:i])) or np.all(np.isin(b[:, j], nums[:i])):
-                tscore = (sum(b.flatten()) -
-                          sum(np.intersect1d(b.flatten(), nums[:i]))) * nums[i - 1]
+                tscore = getScore(b, i)
                 wb[bi] = True
                 if score == None:
                     print('Part 1:', tscore)
