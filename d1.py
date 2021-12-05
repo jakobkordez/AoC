486cc6b626b697d987d8afa7df478ca41443fc33
Jakob Kordež <jakobkordez1999@gmail.com>
2021-12-05 21:51:25 +0100

    Update Day 1

--- d1.py
+++ d1.py
@@ -1,30 +1,5 @@
-# Part 1
-
-# First solution
-c = 0
 with open('i1.txt') as file:
-    inp = list(map(int, file.readlines()))
-for i in range(1, len(inp)):
-    if inp[i] > inp[i-1]:
-        c += 1
-print(c)
+    data = list(map(int, file.readlines()))
 
-# Two-liner
-data = list(map(int, open('i1.txt').readlines()))
-print([data[i-1] < data[i] for i in range(1, len(data))].count(True))
-
-
-# Part 2
-
-# First solution
-c = 0
-with open('i1.txt') as file:
-    inp = list(map(int, file.readlines()))
-for i in range(1, len(inp)-2):
-    if sum(inp[i:i+3]) > sum(inp[i-1:i+2]):
-        c += 1
-print(c)
-
-# Two-liner
-data = list(map(int, open('i1.txt').readlines()))
-print([data[i-3] < data[i] for i in range(3, len(data))].count(True))
+print('Part 1:', [data[i-1] < data[i] for i in range(1, len(data))].count(True))
+print('Part 2:', [data[i-3] < data[i] for i in range(3, len(data))].count(True))
