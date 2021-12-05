66c02df828ae0bde6e81fc3bf6aacd03a463a0a4
Jakob Kordež <jakobkordez1999@gmail.com>
2021-12-05 19:51:28 +0000

    Modify Day 5

--- d5.py
+++ d5.py
@@ -8,13 +8,15 @@ with open('i5.txt') as file:
 s = np.zeros((1000, 1000), dtype=np.int32)
 d = np.zeros((1000, 1000), dtype=np.int32)
 for x1, y1, x2, y2 in data:
-    for i in range(max(abs(y2 - y1) + 1, abs(x2 - x1) + 1)):
-        if y1 == y2:
-            s[y1][min(x1, x2) + i] += 1
-        elif x1 == x2:
-            s[min(y1, y2) + i][x1] += 1
-        else:
-            d[y1 + (i if y1 < y2 else -i)][x1 + (i if x1 < x2 else -i)] += 1
+    xs, xl = sorted([x1, x2])
+    ys, yl = sorted([y1, y2])
+    if y1 == y2:
+        s[y1, xs:xl + 1] += 1
+    elif x1 == x2:
+        s[ys:yl + 1, x1] += 1
+    else:
+        d[ys:yl + 1, xs:xl + 1] += np.rot90(np.identity(
+            xl - xs + 1, dtype=np.int32), 1 if (x1 > x2) != (y1 > y2) else 0)
 
 print('Part 1:', np.sum(s > 1))
 print('Part 2:', np.sum(s + d > 1))
