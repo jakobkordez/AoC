8d6bd626f5a1f6274d82c15b53b2dd7e80f15448
Jakob Kordež <jakobkordez1999@gmail.com>
2021-12-07 12:23:33 +0100

    Optimised Day 7

--- d7.py
+++ d7.py
@@ -1,13 +1,22 @@
 import numpy as np
 
 with open('i7.txt') as file:
-    data = np.array(file.read().split(','), dtype=np.int32)
+    d = np.array(file.read().split(','), dtype=np.int32)
 
 p1 = p2 = np.inf
-for i in range(min(data), max(data) + 1):
-    n = np.abs(data - i)
+for i in range(min(d), max(d) + 1):
+    n = np.abs(d - i)
     p1 = min(p1, np.sum(n))
     p2 = min(p2, np.sum(n*(n+1)//2))
 
 print('Part 1:', p1)
 print('Part 2:', p2)
+
+n = np.array([abs(d-i) for i in range(max(d)+1)])
+print('Part 1:', min(np.sum(n, axis=1).T))
+print('Part 2:', min(np.sum(n * (n+1) // 2, axis=1).T))
+
+# O(n*lb(n))
+print('Part 1:', np.sum(abs(d - np.median(d)), dtype=np.int32))
+n = abs(np.ceil(d - np.average(d)))
+print('Part 2:', np.sum(n*(n+1)//2, dtype=np.int32))
