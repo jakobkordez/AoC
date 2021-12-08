92ddc26b6019d993d880f66c200d06a644d9e54a
Jakob Kordež <jakobkordez1999@gmail.com>
2021-12-08 19:03:32 +0100

    Optimised Day 8

--- d8.py
+++ d8.py
@@ -1,41 +1,23 @@
-from itertools import groupby
+import numpy as np
 
 with open('i8.txt') as file:
     d = [[a.split() for a in l.split(' | ')] for l in file.readlines()]
 
-dd = [0] * 8
-for i, o in d:
-    for oo in o:
-        dd[len(oo)] += 1
 
-print('Part 1:', dd[2] + dd[3] + dd[4] + dd[7])
-
-
-def group(i, key):
-    return {k: list(v) for k, v in groupby(sorted(i, key=key), key=key)}
-
+dd = np.bincount([len(oo) for _, o in d for oo in o], minlength=8)
+print('Part 1:', sum(dd[[2, 3, 4, 7]]))
 
 sm = 0
 for i, o in d:
     i = [set(a) for a in i]
-    nb = [None] * 10
-    nb[8] = next(filter(lambda x: len(x) == 7, i))
-    nb[1] = next(filter(lambda x: len(x) == 2, i))
-    nb[4] = next(filter(lambda x: len(x) == 4, i))
-    nb[7] = next(filter(lambda x: len(x) == 3, i))
+    n = [None] * 10
 
-    s = group(filter(lambda x: x not in nb, i), lambda x: nb[1].issubset(x))
-    # 2 5 6
-    s0 = group(s[False], len)
-    nb[6] = s0[6][0]
-    nb[5] = next(filter(lambda x: x.issubset(nb[6]), s0[5]))
-    nb[2] = next(filter(lambda x: x not in nb, s0[5]))
-    # 0 3 9
-    s1 = group(s[True], len)
-    nb[3] = s1[5][0]
-    nb[9] = next(filter(lambda x: nb[4].issubset(x), s1[6]))
-    nb[0] = next(filter(lambda x: x not in nb, s1[6]))
+    n[1], n[7], n[4], *i, n[8] = sorted(i, key=len)
 
-    sm += int(''.join([str(nb.index(set(a))) for a in o]))
+    h = n[8] - n[4]
+    n[5], n[2], n[6], n[3], n[9], n[0] = \
+        sorted(i, key=lambda x: (x.issuperset(n[1]), len(x), x.issuperset(h)))
+
+    sm += int(''.join([str(n.index(set(a))) for a in o]))
 
 print('Part 2:', sm)
