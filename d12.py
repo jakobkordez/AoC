2cd722bde0225fb8b96d7090a88fb8f6ecdbdcf8
Jakob Kordež <jakobkordez1999@gmail.com>
2021-12-12 18:01:23 +0100

    Clean Day 12

--- d12.py
+++ d12.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 with open('i12.txt') as file:
     m = dict()
     for l in file.readlines():
@@ -8,23 +6,20 @@ with open('i12.txt') as file:
         m.setdefault(b, []).append(a)
 
 stk = []
-std = True
 
 
 def dfs(p: str):
-    global std
     if p == 'end':
         return [1 + len(set(stk)) - len(stk), 1]
     if p.islower():
         if p in stk:
             if len(stk) != len(set(stk)):
-                return (0, 0)
+                return [0, 0]
         stk.append(p)
     r = [dfs(a) for a in m[p] if a != 'start']
-    r = [sum(a for a, _ in r), sum(a for _, a in r)]
     if p.islower():
         stk.pop()
-    return r
+    return [sum(a for a, _ in r), sum(a for _, a in r)]
 
 
 print('Part 1: {}\nPart 2: {}'.format(*dfs('start')))
