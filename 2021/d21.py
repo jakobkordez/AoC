a6aaffb971a56ac7965faf4f515376bcb031d053
Jakob Kordež <jakobkordez1999@gmail.com>
2022-12-05 17:57:44 +0100

    Add day 21 part 2

--- 2021/d21.py
+++ 2021/d21.py
@@ -1,7 +1,8 @@
 from itertools import cycle
 
-p = [9, 6]
+inp = [e-1 for e in [10, 7]]
 
+p = [*inp]
 dr = 0
 s = [0, 0]
 for r in cycle(range(1, 101)):
@@ -15,3 +16,29 @@ for r in cycle(range(1, 101)):
     dr += 1
 
 print('Part 1:', min(s) * (dr + 1))
+
+
+memo = {}
+
+
+def go(p1, p2, s1, s2):
+    mem_key = (p1, p2, s1, s2)
+
+    if s2 >= 21:
+        return 0, 1
+
+    if mem_key not in memo:
+        p1ss = p2ss = 0
+        for x in range(1, 4):
+            for y in range(1, 4):
+                for z in range(1, 4):
+                    np1 = (p1 + x + y + z) % 10
+                    t2, t1 = go(p2, np1, s2, s1 + np1 + 1)
+                    p1ss += t1
+                    p2ss += t2
+        memo[mem_key] = p1ss, p2ss
+
+    return memo[mem_key]
+
+
+print('Part 2:', max(go(*inp, 0, 0)))
