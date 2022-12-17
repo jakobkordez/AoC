a6d28abfe8587b0fd9fa07feb2c575963666ce23
Jakob Kordež <jakobkordez1999@gmail.com>
2022-12-18 00:12:39 +0100

    Update day 16 Add day 17 part 1

--- 2022/d16.py
+++ 2022/d16.py
@@ -1,5 +1,4 @@
 from aoc import *
-from random import randint, random, choice
 
 data = read('i16', ['\n', ' '])
 
@@ -8,113 +7,53 @@ flows = [int(r[4][5:-1]) for r in data]
 tunnels = [[valves.index(t.strip(',')) for t in r[9:]] for r in data]
 
 memo = {}
+solutions = {}
 
 
-def solve(currv, opened, time, mask):
-    if time == 0:
-        return 0
-    key = (currv, time, mask, opened)
+def solve(currv, opened, time, score):
+    if time < 1:
+        solutions[opened] = max(solutions.get(opened, 0), score)
+        return 0, opened
+    key = (currv, time, opened)
 
     if key not in memo:
         flow = flows[currv]
-        mx = 0
         ii = 1 << currv
+        mx = 0
+        bestopen = opened
         if ii & opened == 0 and flow > 0:
             # Turn valve
-            opened |= ii
-            mx = solve(currv, opened, time - 1, mask) + flow * (time-1)
-            opened ^= ii
+            tmx, topen = solve(currv, opened | ii, time -
+                               1, score + flow * (time-1))
+            # tmx += flow * (time-1)
+            if tmx > mx:
+                mx, bestopen = tmx, topen
         # Skip
         for nbr in tunnels[currv]:
-            if (1 << nbr) & mask:
-                mx = max(mx, solve(nbr, opened, time - 1, mask))
-        memo[key] = mx
+            tmx, topen = solve(nbr, opened, time - 1, score)
+            if tmx > mx:
+                mx, bestopen = tmx, topen
+        memo[key] = mx, bestopen
+
+    mx, bestopen = memo[key]
+    solutions[bestopen] = max(solutions.get(bestopen, 0), mx + score)
 
     return memo[key]
 
 
-allm = (1 << len(valves)) - 1
-
 start = valves.index('AA')
 
-print('Part 1:', solve(start, 0, 30, allm))
+solve(start, 0, 30, 0)
+print('Part 1:', max(solutions.values()))
 
+memo.clear()
+solutions.clear()
 
-def evalp2(mask):
-    return solve(start, 0, 26, mask) + solve(start, 0, 26, allm ^ mask)
+solve(start, 0, 26, 0)
+p2 = 0
+for mask1, v1 in solutions.items():
+    for mask2, v2 in solutions.items():
+        if mask1 & mask2 == 0:
+            p2 = max(p2, v1 + v2)
 
-
-# Part 2 genetic algorithm
-best = 0
-pop_size = 100
-cross = 0.2
-mut = 0.2
-pop = [randint(0, allm//2) for _ in range(pop_size)]
-pop = [(evalp2(p), p) for p in pop]
-pop.sort(reverse=True)
-while True:
-    next_pop = pop[:10]
-    next_pop = []
-    nrand = [randint(0, allm//2) for _ in range(10)]
-    next_pop += [(evalp2(p), p) for p in nrand]
-    for p in pop[:-20]:
-        p = p[1]
-        if random() < cross:
-            p2 = choice(pop)[1]
-            bi = randint(0, len(valves) - 1)
-            mask = (1 << bi) - 1
-            p = p & mask | p2 & (allm ^ mask)
-        if random() < mut:
-            p ^= 1 << randint(0, len(valves))
-        next_pop.append((evalp2(p), p))
-    pop = next_pop
-    pop.sort(reverse=True)
-    if pop[0][0] > best:
-        best = pop[0][0]
-        print(best)
-
-
-# Part 2 naive (with memoization)
-@functools.cache
-def solve(currv, curre, opened, time):
-    if time == 0:
-        return 0
-
-    flow = flows[currv]
-    flowe = flows[curre]
-    mx = 0
-    ii = 1 << currv
-    ie = 1 << curre
-    if ii & opened == 0 and flow > 0:
-        # Turn valve
-        opened ^= ii
-        if ie & opened == 0 and flowe > 0:
-            # Elephant turn valve
-            opened ^= ie
-            mx = solve(currv, curre, opened, time - 1) + \
-                (flow + flowe) * time
-            opened ^= ie
-
-        # Elephant skip
-        for nbr in tunnels[curre]:
-            mx = max(mx, solve(currv, nbr, opened,
-                               time - 1) + flow * time)
-
-        opened ^= ii
-
-    if ie & opened == 0 and flowe > 0:
-        # Only Elephant turn valve
-        opened ^= ie
-        for nbr in tunnels[currv]:
-            mx = max(mx, solve(nbr, curre, opened,
-                               time - 1) + flowe * time)
-        opened ^= ie
-
-    # Both Skip
-    for nbr in tunnels[currv]:
-        for nbre in tunnels[curre]:
-            mx = max(mx, solve(nbr, nbre, opened, time - 1))
-    return mx
-
-
-print('Part 2:', solve(start, start, 0, 26))
+print('Part 2:', p2)
---
+++ 2022/d17.py
@@ -0,0 +1,67 @@
+from aoc import *
+from itertools import cycle, islice
+
+W = 7
+
+jets = read('i17')
+
+field = [['.'] * W for _ in range(5000)]
+
+# Bottom-up
+shapes = [
+    ['####'],
+    ['.#.', '###', '.#.'],
+    ['###', '..#', '..#'],
+    [*'####'],
+    ['##', '##']
+]
+
+di = 0
+
+maxy = 0
+for shape in islice(cycle(shapes), 2023):
+    y = maxy + 4
+    x = 2
+    shapeh, shapew = len(shape), len(shape[0])
+    while y > 0:
+        # Jets
+        jetd = jets[di % len(jets)]
+        di += 1
+        dx = 0
+        if jetd == '>' and x < W - shapew:
+            dx = 1
+        elif jetd == '<' and x > 0:
+            dx = -1
+        x += dx
+        # Check collision
+        colides = False
+        for yy in range(shapeh):
+            for xx in range(shapew):
+                if shape[yy][xx] == '#' and field[y + yy][x + xx] == '#':
+                    colides = True
+                    break
+            if colides:
+                break
+        if colides:
+            x -= dx
+        y -= 1
+        # Check collision
+        colides = False
+        for yy in range(shapeh):
+            for xx in range(shapew):
+                if shape[yy][xx] == '#' and field[y + yy][x + xx] == '#':
+                    colides = True
+                    break
+            if colides:
+                break
+        if colides:
+            y += 1
+            break
+    # Place
+    for yy in range(shapeh):
+        for xx in range(shapew):
+            if shape[yy][xx] == '#':
+                field[y + yy][x + xx] = '#'
+    maxy = max(maxy, y + shapeh)
+
+print('Part 1:', maxy)
