13398cf6320ac2a5b8744422b1ec8489220ff96c
Jakob Kordež <jakobkordez1999@gmail.com>
2022-12-19 21:00:01 +0100

    Add day 17 part 2

--- 2022/d17.py
+++ 2022/d17.py
@@ -1,11 +1,10 @@
 from aoc import *
-from itertools import cycle, islice
 
 W = 7
 
 jets = read('i17')
 
-field = [['.'] * W for _ in range(5000)]
+field = [['.'] * W for _ in range(10000)]
 
 # Bottom-up
 shapes = [
@@ -16,11 +15,11 @@ shapes = [
     ['##', '##']
 ]
 
-di = 0
+si = di = 0
 
-maxy = 0
-for shape in islice(cycle(shapes), 2022):
-    y = maxy + 3
+
+def place(shape, y):
+    global di
     x = 2
     shapeh, shapew = len(shape), len(shape[0])
     while y >= 0:
@@ -62,6 +61,43 @@ for shape in islice(cycle(shapes), 2022):
         for xx in range(shapew):
             if shape[yy][xx] == '#':
                 field[y + yy][x + xx] = '#'
-    maxy = max(maxy, y + shapeh)
+    return y
 
-print('Part 1:', maxy)
+
+dct = {}
+
+placed = maxy = 0
+while True:
+    shape = shapes[si % len(shapes)]
+    si += 1
+
+    y = place(shape, maxy + 3)
+
+    maxy = max(maxy, y + len(shape))
+    placed += 1
+
+    if placed == 2022:
+        print('Part 1:', maxy)
+
+    key = (di % len(jets), si % len(shapes))
+    if key in dct and placed > 3000:
+        break
+    dct[key] = placed, maxy
+
+TARGET = 1000000000000
+oldp, oldy = dct[key]
+segBlocks = placed - oldp
+segHeight = maxy - oldy
+segCount = (TARGET - placed) // segBlocks
+placed += segCount * segBlocks
+
+while placed < TARGET:
+    shape = shapes[si % len(shapes)]
+    si += 1
+
+    y = place(shape, maxy + 3)
+
+    maxy = max(maxy, y + len(shape))
+    placed += 1
+
+print('Part 2:', maxy + segHeight * segCount)
