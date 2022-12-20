106073b8327bbf9dc0c77ef4c9634eddd7b08597
Jakob Kordež <jakobkordez1999@gmail.com>
2022-12-20 21:28:59 +0100

    Improve day 20

--- 2022/d20.py
+++ 2022/d20.py
@@ -4,41 +4,62 @@ origCoords = read('i20', ['\n'], int)
 N = len(origCoords)
 
 
-def solve(part2):
-    decrKey = 811589153 if part2 else 1
-    coords = [[x*decrKey, None, None] for x in origCoords]
-    zeroi = coords.index([0, None, None])
-    for i in range(N):
-        coords[i][1] = coords[(i - 1) % N]
-        coords[i][2] = coords[(i + 1) % N]
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.prev: Node = None
+        self.next: Node = None
 
-    rounds = 10 if part2 else 1
+    def nextN(self, n):
+        curr = self
+        for _ in range(n):
+            curr = curr.next
+        return curr
+
+    def prevN(self, n):
+        curr = self
+        for _ in range(n):
+            curr = curr.prev
+        return curr
+
+
+def solve(decrKey, rounds):
+    coords = [Node(x * decrKey) for x in origCoords]
+    for i in range(N):
+        coords[i].prev = coords[(i - 1) % N]
+        coords[i].next = coords[(i + 1) % N]
+
     for _ in range(rounds):
-        for i in range(N):
-            curr = coords[i]
-            pr = curr[1]
-            nx = curr[2]
-            pr[2] = nx
-            nx[1] = pr
-            for _ in range(abs(curr[0]) % (N - 1)):
-                if curr[0] > 0:
-                    nx = nx[2]
-                else:
-                    nx = nx[1]
-            pr = nx[1]
-            nx[1] = curr
-            curr[2] = nx
-            curr[1] = pr
-            pr[2] = curr
+        for curr in coords:
+            # Remove curr
+            pr = curr.prev
+            nx = curr.next
+            pr.next = nx
+            nx.prev = pr
+
+            # Find destination
+            val = abs(curr.val) % (N - 1)
+            if curr.val > 0:
+                nx = curr.nextN(val)
+            else:
+                nx = curr.prevN(val)
+            pr = nx.prev
+
+            # Insert curr
+            nx.prev = curr
+            curr.next = nx
+            curr.prev = pr
+            pr.next = curr
 
     sol = 0
-    curr = coords[zeroi]
-    for i in range(3):
-        for _ in range(1000 % N):
-            curr = curr[2]
-        sol += curr[0]
+    for curr in coords:
+        if curr.val == 0:
+            break
+    for _ in range(3):
+        curr = curr.nextN(1000 % N)
+        sol += curr.val
     return sol
 
 
-print('Part 1:', solve(False))
-print('Part 2:', solve(True))
+print('Part 1:', solve(1, 1))
+print('Part 2:', solve(811589153, 10))
