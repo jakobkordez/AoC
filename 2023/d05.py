b6df7d3b678d892b9eef4f2d9b326526516e9294
Jakob Kordež <jakobkordez1999@gmail.com>
2023-12-05 17:39:33 +0100

    Change day 5

--- 2023/d05.py
+++ 2023/d05.py
@@ -17,29 +17,29 @@ def solve(seedR):
     for step in steps:
         newRanges = []
         while ranges:
-            rStart, rLen = ranges.pop()
-            rEnd = rStart + rLen
+            rStart, rEnd = ranges.pop()
 
             for dStart, sStart, dsLen in step:
                 oStart = max(rStart, sStart)
                 oEnd = min(rEnd, sStart + dsLen)
                 if oStart >= oEnd:
                     continue
+                d = dStart - sStart
 
-                newRanges.append([dStart + oStart - sStart, oEnd - oStart])
+                newRanges.append([oStart + d, oEnd + d])
                 if oStart != rStart:
-                    ranges.append([rStart, oStart - rStart])
+                    ranges.append([rStart, oStart])
                 if oEnd != rEnd:
-                    ranges.append([oEnd, rEnd - oEnd])
+                    ranges.append([oEnd, rEnd])
 
                 break
             else:
-                newRanges.append([rStart, rLen])
+                newRanges.append([rStart, rEnd])
 
         ranges = deque(newRanges)
 
     return min(ranges)[0]
 
 
-print("Part 1:", solve(map(lambda x: [x, 1], seeds)))
-print("Part 2:", solve(splitByCount(seeds, 2)))
+print("Part 1:", solve(map(lambda x: [x, x + 1], seeds)))
+print("Part 2:", solve(map(lambda x: [x[0], sum(x)], splitByCount(seeds, 2))))
