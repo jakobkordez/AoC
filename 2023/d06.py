0de7440afc009729678ee3675aa30d3120bf47f4
Jakob Kordež <jakobkordez1999@gmail.com>
2023-12-06 16:59:33 +0100

    Fix day 6

--- 2023/d06.py
+++ 2023/d06.py
@@ -1,26 +1,26 @@
 from aoc import *
 import re
-from math import prod, ceil, sqrt
+from math import prod, floor, sqrt
 
 
 # O(t) brute force
 def solveBF(t, p):
-    return sum(i * (t - i) >= p for i in range(1, t))
+    return sum(i * (t - i) > p for i in range(1, t))
 
 
 # O(t) faster brute force
 def solveBF2(t, p):
-    return t - 2 * next(i for i in range(1, t) if i * (t - i) >= p) + 1
+    return t - 2 * next(i for i in range(1, t) if i * (t - i) > p) + 1
 
 
 # O(log(t)) binary search
 def solveBS(t, p):
-    return t - 2 * binarySearch(1, t // 2, lambda m: m * (t - m) >= p) + 1
+    return t - 2 * binarySearch(1, t // 2, lambda m: m * (t - m) > p) + 1
 
 
 # O(log(t)) because of sqrt
 def solveMath(t, p):
-    return t - 2 * ceil((t - sqrt(t * t - 4 * p)) / 2) + 1
+    return t - 2 * floor((t - sqrt(t * t - 4 * p)) / 2) - 1
 
 
 data = zip(*read("i06", ["\n", re.compile(r"\d+")], int))
