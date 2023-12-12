668dd0e994be4c5fa8bec09b26152a7ba0d57a5f
Jakob Kordež <jakobkordez1999@gmail.com>
2023-12-12 12:11:04 +0100

    Minor day 12 optimization

--- 2023/d12.py
+++ 2023/d12.py
@@ -6,32 +6,28 @@ data = read(12, ["\n", " "])
 
 
 @cache
-def go(d: str, v: tuple):
-    if len(v) == 0:
+def go(d: str, v: str):
+    if v == "":
         return 1 if "#" not in d else 0
     if d == "":
         return 0
     c = 0
     if d[0] != "#":
         c += go(d[1:].strip("."), v)
-    vv, *sv = v
-    i = d.find(".")
-    if i == -1:
-        i = len(d)
-    if i < vv:
+    vv, sv = v.split(",", 1)
+    vv = int(vv)
+    if d.find(".") % (len(d) + 1) < vv:
         return c
     if vv == len(d) or d[vv] != "#":
-        c += go(d[vv + 1 :].strip("."), tuple(sv))
+        c += go(d[vv + 1 :].strip("."), sv)
     return c
 
 
 p1 = p2 = 0
 for d, v in data:
-    v = [*map(int, v.split(","))]
-    p1 += go(d.strip("."), tuple(v))
-
-    v = chain(*repeat(v, 5))
-    p2 += go("?".join([d] * 5).strip("."), tuple(v))
+    v = f"{v},"
+    p1 += go(d.strip("."), v)
+    p2 += go("?".join([d] * 5).strip("."), v * 5)
 
 print("Part 1:", p1)
 print("Part 2:", p2)
