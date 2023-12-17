50553368cd15cfd616f57a2ff46a9a3157391263
Jakob Kordež <jakobkordez1999@gmail.com>
2023-12-17 19:46:55 +0100

    Optimize day 17

--- 2023/d17.py
+++ 2023/d17.py
@@ -8,28 +8,34 @@ H, W = len(data), len(data[0])
 
 def solve(max_s, min_s: int = 0, data=data, H=H, W=W):
     seen = set()
-    q = [(0, 1, 0, 1, 1), (0, 0, 1, 2, 1)]
+    q = [(0, 1, 0, 1), (0, 0, 1, 2)]
     heapq.heapify(q)
     while q:
-        d, x, y, dr, t = heapq.heappop(q)
+        d, x, y, dr = heapq.heappop(q)
         if x < 0 or x >= W or y < 0 or y >= H:
             continue
-        if (x, y, dr, t) in seen:
-            continue
-        seen.add((x, y, dr, t))
-        d += data[y][x]
-        if x == W - 1 and y == H - 1 and t >= min_s:
+        if dr == -1:
             return d
-        if t < max_s:
-            dx, dy = FOUR_NEIGHBOURS[dr]
-            heapq.heappush(q, (d, x + dx, y + dy, dr, t + 1))
-        if t >= min_s:
-            lft = (dr - 1) % 4
-            dx, dy = FOUR_NEIGHBOURS[lft]
-            heapq.heappush(q, (d, x + dx, y + dy, lft, 1))
-            rgt = (dr + 1) % 4
-            dx, dy = FOUR_NEIGHBOURS[rgt]
-            heapq.heappush(q, (d, x + dx, y + dy, rgt, 1))
+        if (x, y, dr) in seen:
+            continue
+        seen.add((x, y, dr))
+        dx, dy = FOUR_NEIGHBOURS[dr]
+        for dist in range(1, max_s + 1):
+            if x < 0 or x >= W or y < 0 or y >= H:
+                break
+            d += data[y][x]
+            if dist >= min_s:
+                if x == W - 1 and y == H - 1:
+                    heapq.heappush(q, (d, x, y, -1))
+                    break
+                lft = (dr - 1) % 4
+                fx, fy = FOUR_NEIGHBOURS[lft]
+                heapq.heappush(q, (d, x + fx, y + fy, lft))
+                rgt = (dr + 1) % 4
+                fx, fy = FOUR_NEIGHBOURS[rgt]
+                heapq.heappush(q, (d, x + fx, y + fy, rgt))
+            x += dx
+            y += dy
 
 
 print("Part 1:", solve(3))
