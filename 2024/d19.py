2ec46273fc46a0373f328ea044b4bfeab9a4a826
Jakob Kordež <jakobkordez1999@gmail.com>
2024-12-20 21:39:15 +0100

    Optimize day 19

--- 2024/d19.py
+++ 2024/d19.py
@@ -5,15 +5,37 @@ from functools import cache
 towels, patterns = read(19, ["\n\n", re.compile("[a-z]+")])
 
 
+class Trie:
+    def __init__(self):
+        self.c: dict[str, Trie] = dict()
+        self.e = False
+
+    def add(self, s: str):
+        if s == "":
+            self.e = True
+        else:
+            self.c.setdefault(s[0], Trie())
+            self.c[s[0]].add(s[1:])
+
+    def get(self, s: str):
+        ret = []
+        if s != "" and s[0] in self.c:
+            ret.extend(e + 1 for e in self.c[s[0]].get(s[1:]))
+        if self.e:
+            ret.append(0)
+        return ret
+
+
+root = Trie()
+for t in towels:
+    root.add(t)
+
+
 @cache
 def solve(pattern: str):
     if pattern == "":
         return 1
-    sm = 0
-    for t in towels:
-        if pattern.startswith(t):
-            sm += solve(pattern[len(t) :])
-    return sm
+    return sum(solve(pattern[t:]) for t in root.get(pattern))
 
 
 r = list(map(solve, patterns))
